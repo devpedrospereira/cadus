624248ed6050b82387d4971b76368d4981d1ffce
DevPedroPereira <pedros.pereira558@gmail.com>
2022-11-12 19:26:40 -0300

    initilized project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import { StartupSection } from "./Index";
+
+//components
+
+function App() {
+  return (
+    <div className="App">
+      <StartupSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Index.tsx
@@ -0,0 +1,9 @@
+import { Header } from "./components/Header/Header";
+
+export const StartupSection = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
